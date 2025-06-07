1f70f3b73725aab95dacb16a803172ce8abf4443
Ford Berry <fordaberry@gmail.com>
2025-06-06 19:42:37 -0600

    Initial commit
    Create functions for addition, subtraction, multiplication and division.

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b) {
+    return a + b;
+}
+
+// add(4,6);
+
+function subtract(a,b) {
+    return a - b;
+}
+
+// subtract(10,4);
+
+function multiply(a,b) {
+    return a * b;
+}
+
+// multiply(4,4);
+
+function divide(a,b) {
+    return a / b;
+}
+
+// divide(10, 5);
