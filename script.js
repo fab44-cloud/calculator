7942ede4b4b7833e9f4dbdc3b7072aac195c5f79
Ford Berry <fordaberry@gmail.com>
2025-06-20 20:52:47 -0600

    Cut code for minus as unary operator because it was not being reached

--- script.js
+++ script.js
@@ -86,15 +86,6 @@ function handleOperandInput(input) {
     }
     
     if (!operatorSelected) {
-        // Handle negative sign for the first operand
-        if (firstOperand === "" && input === "-") {
-            console.log(firstOperand);
-            firstOperand += input;
-            updateDisplay(firstOperand);
-            console.log(`First operand: ${firstOperand}`);
-            return;
-        }
-
         // Handle regular digit input for the first operand
         firstOperand += input;
         if (firstOperand.length < 13) {
@@ -102,16 +93,9 @@ function handleOperandInput(input) {
             console.log(`First operand: ${firstOperand}`);  
         }
     } else {
-        // Handle negative sign for the second operand
-        if (secondOperand === "" && secondOperand === "-") {
-            secondOperand += input;
-            updateDisplay(secondOperand);
-            console.log(`Second operand: ${secondOperand}`);
-            return;
-        }
-
         // Handle regular digit input for the second operand
         secondOperand += input;
+        console.log(secondOperand)
         if (secondOperand.length < 13) {
             updateDisplay(secondOperand);
             console.log(`Second operand: ${secondOperand}`);
@@ -140,7 +124,7 @@ function handleOperatorInput(input) {
 
     if (!operatorSelected) {
         // Store the operator
-        operator = input;
+        operator += input;
         console.log(`Operator: ${operator}`);
         // Display the operator
         updateDisplay(operator);
